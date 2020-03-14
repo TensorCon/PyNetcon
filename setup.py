0b0557f55e2a54c81b08b96dab496b4ee87d669e
David Anekstein <aneksteind@gmail.com>
2020-03-14 11:50:06 -0400

    initial commit calling netcon

---
+++ setup.py
@@ -0,0 +1,42 @@
+import shutil
+import os
+
+from distutils.command.sdist import sdist as sdist_orig
+from distutils.errors import DistutilsExecError
+
+from setuptools import setup  
+
+import setuptools.command.build_py
+
+
+class BuildPyCommand(setuptools.command.build_py.build_py):
+  """Custom build command."""
+
+  def run(self):    
+    netcon_dir = 'netcon/'
+    mex_file = 'netcon_nondisj_cpp.mex'
+    object_file = 'netcon_nondisj_cpp.o'
+    try:
+        self.spawn(['mkoctfile', '--mex', '-O3', 'netcon/netcon_nondisj_cpp.cpp'])
+        if os.path.exists(netcon_dir + mex_file):
+            os.remove(netcon_dir + mex_file)
+        if os.path.exists(netcon_dir + object_file):
+            os.remove(netcon_dir + object_file)
+        shutil.move(mex_file, netcon_dir)
+        shutil.move(object_file, netcon_dir)
+    except DistutilsExecError:
+        self.warn('mkoctfile failed. Have you installed octave?')
+    setuptools.command.build_py.build_py.run(self)
+
+
+setup(name='spam',
+    version='0.1',
+    packages=['pynetcon'],
+    cmdclass={
+        'build_py': BuildPyCommand
+    },
+    install_requires=[
+        'oct2py==4.0.6'
+    ],
+    data_files={'netcon':['netcon/netcon.m', 'netcon/netcon_nondisj_cpp.cpp']},
+)
